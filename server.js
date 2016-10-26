993c4079d15a4b36277739360bd678a122cc9e56
Matt Bedell <matt@bedell.co>
2016-10-26 19:07:00 -0400

    tweet data test succseful

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const app = express();
 const logger = require('morgan');
 const Oauth = require('OAuth')
 require('dotenv').config();
-const OAuth2 = Oauth.OAuth2;
+const tClient = require('twitter');
+app.use(logger('dev'));
+
+var twitClient = new tClient({
+  consumer_key: `${process.env.API_KEY}`,
+  consumer_secret: `${process.env.API_SECRET}`,
+  bearer_token: `${process.env.API_BEARER}`
+});
+
+//const OAuth2 = Oauth.OAuth2;
+twitClient.get('search/tweets', {q:'trump'}, (error, tweets, response) => {
+  if(error) throw error;
+  //console.log(response);
+  tweets.statuses.forEach( (obj) => {console.log(obj.user);});
+});
 const PORT = process.env.PORT || 3000;
-const app = express();
+
+
+
+
+
+
+
+
+
+
+
 
 // const oauth2 = new OAuth2(
 //    process.env.API_KEY,
