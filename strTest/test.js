f7ed7c8d754e4c5063a665afa75fe85ff9a71726
Matt Bedell <matt@bedell.co>
2016-10-28 10:25:28 -0400

    add regex to extract links and hashtags for text body

--- strTest/test.js
+++ strTest/test.js
@@ -1,20 +1,24 @@
 console.log("LOADED");
 let mystr = 'RT @gntlman: TRUMP on Track to Win More Black Votes Than Any GOP Candidate Since 1960 https://t.co/LuXlBslIng'
+let myotherstr = 'RT @JohnFromCranber: ....They\'re Preparing to Flee The Country if #Hillary Loses, Because an Honest Trump DOJ Will Put Them in Jail? #tcot…'
+let myotherstr2 = 'POLL: Trump Voters Want The 1950s Back https://t.co/CHBDKVsVcg via @YouTube';
 
-let mysplit = mystr.split(/(http*\S*)/g);
 
-///(http*\S*)/g
-let myDiv = document.querySelector('.displayText');
-const checkLink = (myArr) => {
-  let confirmedLink;
-  myArr.forEach( (myVal) => {
-    if(myVal.indexOf('http', 0) === 0) {
-      let confirmedLink = myVal;
-      //console.log(confirmedLink);
-    }
-  })
-  return confirmedLink;
+const cleanText = (str) => {
+  let originalText = str;
+
+  let extractedLinks = str.match(/(http*\S*)/g);
+  let extractedHashtags = str.match(/(# *\S*)/g);
+  let extractedMentions = str.match(/(@ *\S*)/g);
+
+  let cleanedText = str.replace(/(http*\S*)/g, '');//Clean links
+  cleanedText = cleanedText.replace(/(# *\S*)/g);//Clean hastags
+  cleanedText = cleanedText.replace(/(@ *\S*)/g, '');//Clean mentions
+
+  return { cleanedText, originalText, extractedHashtags, extractedLinks, extractedMentions };
 }
-let displaythis = checkLink(mysplit);
-//myDiv.innerHTML = displaythis;
-console.log(displaythis);
+
+
+
+let myclean = cleanText(myotherstr2);
+console.log(myclean);
