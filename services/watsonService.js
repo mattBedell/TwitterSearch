bb68776e5aa41d4bc03459afb64f2dd40a3153ee
Matt Bedell <matt@bedell.co>
2016-10-31 18:51:15 -0400

    tweaks to styling

--- services/watsonService.js
+++ services/watsonService.js
@@ -6,7 +6,7 @@ module.exports = function watsonService () {
     const API_URL = `https://gateway-a.watsonplatform.net/calls/text/TextGetCombinedData?apikey=${apiKey}&text="${req.body.submitedTweet.text}"&showSourceText=1&extract=doc-sentiment,doc-emotion,concepts&outputMode=json`;
     fetch(API_URL).then((r)=> r.json())
     .then((result) => {
-      res.analysis = result;
+      res.analysis = result || [];
       res.tweet = req.body.submitedTweet;
       next();
       console.log(result);
